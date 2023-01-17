80976b13e81315f45f908fc574c9c0abcfe63b68
Timothée Rebours <tim@seald.io>
2023-01-17 17:13:23 +0100

    stop using nuxt-picture see #2

--- pages/index.vue
+++ pages/index.vue
@@ -7,7 +7,7 @@
       <div class="inline-block flex-shrink-0 flex-grow-0 max-w-sm relative mt-20 mx-16 md:mx-0" >
         <div class="rounded-b-full aspect-[10/11] relative overflow-hidden" >
             <div class="w-4/5 ml-[11%]">
-              <nuxt-picture preload src="IMG-20220515-WA0004-removebg-preview.png"/>
+              <nuxt-img preload src="IMG-20220515-WA0004-removebg-preview.png"/>
             </div>
         </div>
         <div class="absolute bottom-0 left-0 rounded-full aspect-square bg-gradient-to-t from-gray-900 to-gray-500 w-full overflow-hidden -z-50" ></div>
