abce32a672b0bbeb69c9d79b38363fc54a1d46bc
Timothée Rebours <tim@seald.io>
2024-06-10 16:44:43 +0200

    WIP

--- src/components/og-image.tsx
+++ src/components/og-image.tsx
@@ -4,6 +4,7 @@ import { findLargestUsableFontSize } from '@altano/satori-fit-text'
 import roboto400Normal from '@fontsource/roboto/files/roboto-latin-400-normal.woff?arraybuffer'
 import roboto700Normal from '@fontsource/roboto/files/roboto-latin-700-normal.woff?arraybuffer'
 import { waterfall } from 'astro-pintora'
+import type { OGImageOptions } from '~/utils/createImage.ts'
 
 // TODO: wrapping with waterfall from astro-pintora is due to the fact that @pintora/cli pollutes the globals with
 //  window, document, etc. which come from JSDom, it cleans up after each run, however, @altano/satori-fit-text tests
@@ -12,7 +13,16 @@ import { waterfall } from 'astro-pintora'
 //  will fail. Using the waterfall makes sure they wait for each other to finish.
 //  see: https://github.com/hikerpig/pintora/issues/237 and
 
-export default waterfall(async ({ image, title, description, author, date, width = 1200, height = 630, dateFormat = 'en' }: { image: ArrayBuffer, title: string, description: string, width?: number, height?: number, author?: string, date?: Date, dateFormat?: string }) => {
+export default waterfall(async ({
+  image,
+  title,
+  description,
+  author,
+  date,
+  width = 1200,
+  height = 630,
+  dateFormat = 'en'
+}: OGImageOptions) => {
   const resizedImage = await sharp(Buffer.from(image))
     .resize({ width: Math.floor(width / 3), height: 1.25 * Math.floor(width / 3), fit: 'cover' })
     .png()
@@ -51,14 +61,63 @@ export default waterfall(async ({ image, title, description, author, date, width
   })
 
   return <div style={{ display: 'flex', padding: frameWidth, width, height }}>
-    <div style={{ display: 'flex', maxWidth: '100%', maxHeight: '100%', flexDirection: 'column', backgroundColor: 'white', justifyContent: 'center', borderRadius: 16, paddingRight: imageMargin, boxShadow: '0 25px 50px -12px rgb(0 0 0 / 0.25)', width, height }}>
-      <div style={{ display: 'flex', maxWidth: '100%', maxHeight: '100%', width: width - 2 * frameWidth, height: height - 2 * frameWidth }}>
+    <div style={{
+      display: 'flex',
+      maxWidth: '100%',
+      maxHeight: '100%',
+      flexDirection: 'column',
+      backgroundColor: 'white',
+      justifyContent: 'center',
+      borderRadius: 16,
+      paddingRight: imageMargin,
+      boxShadow: '0 25px 50px -12px rgb(0 0 0 / 0.25)',
+      width,
+      height
+    }}>
+      <div style={{
+        display: 'flex',
+        maxWidth: '100%',
+        maxHeight: '100%',
+        width: width - 2 * frameWidth,
+        height: height - 2 * frameWidth
+      }}>
         <img style={{ margin: imageMargin }} src={b64ImageSrc} width={Math.floor(width / 3)}/>
         <div style={{ display: 'flex', flexDirection: 'column', width: contentWidth, height: height - 2 * frameWidth }}>
-          <h1 style={{ display: 'block', position: 'absolute', left: 0, top: 0, fontSize: titleFontSize, fontWeight: 700, maxWidth: contentWidth, lineClamp: 2, lineHeight: 1.3 }}>{title}</h1>
-          <span style={{ display: 'block', position: 'absolute', left: 0, top: titleMaxHeight + imageMargin, fontSize: descriptionFontSize, fontWeight: 400, maxWidth: contentWidth, lineClamp: 3, lineHeight: 1.3 }}>{description}</span>
-          {author != null ? <span style={{ position: 'absolute', bottom: 0, left: 0, fontSize: 30, marginBottom: imageMargin }}>{author}</span> : <></>}
-          {dateString != null ? <span style={{ position: 'absolute', bottom: imageMargin, right: 0, fontSize: 30 }}>{dateString}</span> : <></>}
+          <h1 style={{
+            display: 'block',
+            position: 'absolute',
+            left: 0,
+            top: 0,
+            fontSize: titleFontSize,
+            fontWeight: 700,
+            maxWidth: contentWidth,
+            lineClamp: 2,
+            lineHeight: 1.3
+          }}>{title}</h1>
+          <span style={{
+            display: 'block',
+            position: 'absolute',
+            left: 0,
+            top: titleMaxHeight + imageMargin,
+            fontSize: descriptionFontSize,
+            fontWeight: 400,
+            maxWidth: contentWidth,
+            lineClamp: 3,
+            lineHeight: 1.3
+          }}>{description}</span>
+          {author != null
+            ? <span style={{
+              position: 'absolute',
+              bottom: 0,
+              left: 0,
+              fontSize: 30,
+              marginBottom: imageMargin
+            }}>{author}</span>
+            : <></>}
+          {dateString != null
+            ? <span
+            style={{ position: 'absolute', bottom: imageMargin, right: 0, fontSize: 30 }}>{dateString}</span>
+            : <></>}
         </div>
       </div>
     </div>
--- src/utils/createImage.ts
+++ src/utils/createImage.ts
@@ -3,7 +3,28 @@ import roboto400Normal from '@fontsource/roboto/files/roboto-latin-400-normal.wo
 import roboto700Normal from '@fontsource/roboto/files/roboto-latin-700-normal.woff?arraybuffer'
 import { Resvg } from '@resvg/resvg-js'
 import type { ReactNode } from 'react'
-import type { Buffer } from 'node:buffer'
+import { Buffer } from 'node:buffer'
+import { createHash } from 'node:crypto'
+import path from 'node:path'
+import AstroConfig, { PWD } from '../../astro.config.ts'
+import { existsSync } from 'node:fs'
+import { mkdir, writeFile } from 'node:fs/promises'
+import ogImage from '~/components/og-image.tsx'
+
+export interface OGImageOptions {
+  image: ArrayBuffer
+  title: string
+  description: string
+  width?: number
+  height?: number
+  author?: string
+  date?: Date
+  dateFormat?: string
+}
+
+const PUBLIC_DIR = path.resolve(PWD, AstroConfig.publicDir ?? 'public')
+const OG_DIR = path.resolve(PUBLIC_DIR, 'og')
+if (!(existsSync(OG_DIR))) await mkdir(OG_DIR)
 
 export const SVG = async (component: ReactNode): Promise<string> => {
   return await satori(component, {
@@ -36,4 +57,24 @@ export const PNG = async (component: ReactNode): Promise<Buffer> => {
   })
   const pngData = resvg.render()
   return pngData.asPng()
+}
+const generateHash = (input: string): string => {
+  const hash = createHash('sha256')
+  hash.update(input)
+  return hash.digest().toString('hex')
+}
+
+export const getCachedImage = async (options: OGImageOptions): Promise<string> => {
+  const image = Buffer.from(options.image)
+
+  const hash = generateHash(JSON.stringify({ ...options, image: image.toString('base64') }))
+
+  const generatedImagePath = path.resolve(OG_DIR, `${hash}.png`)
+
+  if (!(existsSync(generatedImagePath))) {
+    const generatedImage = await PNG(await ogImage({ ...options, image }))
+    await writeFile(generatedImagePath, generatedImage)
+  }
+
+  return `/og/${hash}.png`
 }
